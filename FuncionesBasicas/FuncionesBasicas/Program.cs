de293e014737852bfc8a28498313f7f58e6de2ac
Isabel Encina <117072817+IzziEnso@users.noreply.github.com>
2023-03-19 23:06:39 +0100

    Add files via upload
    Práctica 4.3 Tipos de datos,  se compone de dos aplicaciones de consola. La primera es de operaciones básicas y la segunda de funciones básicas.

---
+++ FuncionesBasicas/FuncionesBasicas/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ProgramaFuncionesBasicas
+{
+    class Program
+    {
+        static void Main(String[] Args)
+        {
+            double num1, num2;
+
+            Console.Write("DIGITE UN PRIMER NÚMERO: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("DIGITE UN SEGUNDO NÚMERO: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            //VALOR ABSOLUTO
+            Console.WriteLine($"VALOR ABSOLUTO PRIMER NÚMERO: {Math.Abs(num1)}");
+            Console.WriteLine($"VALOR ABSOLUTO SEGUNDO NÚMERO: {Math.Abs(num2)}");
+
+            //POTENCIA
+            Console.WriteLine($"POTENCIA PRIMER NÚMERO: {Math.Pow(num1, 3)}");
+            Console.WriteLine($"POTENCIA SEGUNDO NÚMERO: {Math.Pow(num2, 3)}");
+
+            //RAIZ CUADRADA
+            Console.WriteLine($"RAIZ CUADRADA PRIMER NÚMERO: {Math.Sqrt(num1)}");
+            Console.WriteLine($"RAIZ CUADRADA SEGUNDO NÚMERO: {Math.Sqrt(num2)}");
+
+            //SENO
+            Console.WriteLine($"SENO PRIMER NÚMERO: {Math.Sin(num1 * Math.PI / 180)}");
+            Console.WriteLine($"SENO SEGUNDO NÚMERO: {Math.Sin(num2 * Math.PI / 180)}");
+
+            //COSENO
+            Console.WriteLine($"COSENO PRIMER NÚMERO: {(num1 * Math.PI / 180)}");
+            Console.WriteLine($"COSENO SEGUNDO NÚMERO: {(num2 * Math.PI / 180)}");
+
+            //NÚMERO MÁXIMO 
+            Console.WriteLine($"NÚMERO MÁXIMO PRIMER NÚMERO: {Math.Max(num1, 50)}");
+            Console.WriteLine($"NÚMERO MÁXIMO SEGUNDO NÚMERO: {Math.Max(num2, 50)}");
+
+            //NÚMERO MÍNIMO
+            Console.WriteLine($"NÚMERO MÍNIMO PRIMER NÚMERO: {Math.Min(num1, 50)}");
+            Console.WriteLine($"NÚMERO MÍNIMO SEGUNDO NÚMERO: {Math.Min(num2, 50)}");
+
+            //PARTE ENTERA
+            Console.WriteLine($"PARTE ENTERA PRIMER NÚMERO: {Math.Truncate(num1)}");
+            Console.WriteLine($"PARTE ENTERA SEGUNDO NÚMERO: {Math.Truncate(num2)}");
+
+            //REDONDEO
+            Console.WriteLine($"REDONDEO PRIMER NÚMERO: {Math.Round(num1)}");
+            Console.WriteLine($"REDONDEO SEGUNDO NÚMERO: {Math.Round(num2)}");
+
+            Console.WriteLine("\n\n\tPresione una tecla para salir ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TiposDatos/TiposDatos/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ProgramaOperacionesBasicas
+{
+    class Program
+    {
+        static void Main (String[] Args)
+        {
+            int num1, num2, suma, resta, division, multiplicacion, residuo;
+
+            Console.Write("PRIMER NÚMERO: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("SEGUNDO NÚMERO: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            //SUMA
+            suma = num1 + num2;
+            Console.WriteLine("LA SUMA ES: " + suma);
+
+            //RESTA
+            resta = num1 - num2;
+            Console.WriteLine("LA RESTA ES: " + resta);
+
+            //MULTIPLICACION
+            multiplicacion = num1 * num2;
+            Console.WriteLine("LA MULTIPLICACIÓN ES: " + multiplicacion);
+
+            //DIVISIÓN
+            if (num2 != 0)
+            {
+                division = num1 / num2;
+                Console.WriteLine("LA DIVISIÓN ES: " + division);
+
+            }
+
+            //RESIDUO
+            residuo = num1 % num2;
+            Console.WriteLine("EL RESIDUO ES: " + residuo);
+
+
+            Console.WriteLine("\n\n\tPresione una tecla para salir ...");
+            Console.ReadKey();
+
+        }
+    }
+}
